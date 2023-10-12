3da8cdde7bcd0434b7fd7bf4d15f53faa9e349f6
miazigoo <kasper6024@gmail.com>
2023-10-12 20:09:03 +0300

    add DialogFlow to VK bot

--- tgbot.py
+++ tgbot.py
@@ -2,8 +2,8 @@ from aiogram import Bot, types, Dispatcher, F
 import asyncio
 from aiogram.filters import Command
 from environs import Env
-from google.cloud import dialogflow
 
+from flow_utils import detect_intent_texts
 
 env = Env()
 env.read_env()
@@ -13,22 +13,6 @@ PROJECT_ID = env.str("PROJECT_ID")
 
 bot = Bot(token=TGTOKEN)
 dp = Dispatcher()
-
-
-def detect_intent_texts(project_id, session_id, text, language_code='ru'):
-    session_client = dialogflow.SessionsClient()
-
-    session = session_client.session_path(project_id, session_id)
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
-    query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    return response.query_result.fulfillment_text
-
 
 
 @dp.message(Command("start"))
--- vkchat.py
+++ vkchat.py
@@ -4,16 +4,24 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 import random
 import vk_api as vk
 
+from flow_utils import detect_intent_texts
+
 env = Env()
 env.read_env()
 
 VK_API_KEY = env.str("VK_API_KEY")
+PROJECT_ID = env.str("PROJECT_ID")
 
 
 def echo(event, vk_api):
+    answer_text = detect_intent_texts(
+        PROJECT_ID,
+        event.user_id,
+        event.text
+    )
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=answer_text,
         random_id=random.randint(1, 1000)
     )
 

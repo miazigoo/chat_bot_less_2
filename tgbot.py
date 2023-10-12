7754ee813854886bf039986ec7b32b5308da21ba
miazigoo <kasper6024@gmail.com>
2023-10-12 18:57:31 +0300

    setup DialogFlow

--- tgbot.py
+++ tgbot.py
@@ -1,16 +1,34 @@
 from aiogram import Bot, types, Dispatcher, F
 import asyncio
-
 from aiogram.filters import Command
 from environs import Env
+from google.cloud import dialogflow
+
 
 env = Env()
 env.read_env()
 
 TGTOKEN = env.str("TGTOKEN")
+PROJECT_ID = env.str("PROJECT_ID")
 
 bot = Bot(token=TGTOKEN)
 dp = Dispatcher()
+
+
+def detect_intent_texts(project_id, session_id, text, language_code='ru'):
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
+
 
 
 @dp.message(Command("start"))
@@ -25,7 +43,12 @@ async def process_help_command(message: types.Message):
 
 @dp.message(F.text)
 async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text)
+    answer_text = detect_intent_texts(
+        PROJECT_ID,
+        msg.from_user.id,
+        msg.text
+    )
+    await bot.send_message(msg.from_user.id, answer_text)
 
 
 async def main():
